c1d547aceac29a771dcbde2a3bfd0d8515b64d64
kirstiegeddes <kirstie.geddes@stoneacre.co.uk>
2017-10-24 16:07:02 +0100

    header and nav finished

--- assets/js/lib/nav.js
+++ assets/js/lib/nav.js
@@ -1,4 +1,16 @@
- $(function(){ 
-    $( ".jsHamburger" ).toggle() ;
- });
-    
+  
+$(function(){ 
+    $('.jsHamburger').on('click', function(event) {
+        event.preventDefault();
+        
+        $('nav').toggle();
+        $('.menu').toggleClass('mobileNav');
+    });
+});
+
+
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
